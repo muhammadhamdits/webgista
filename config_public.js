ce75d2f54d15af2e4daa697bff4e12a6782d3988
MUHAMMAD HAMDI <1711522014@student.unand.ac.id>
2019-10-18 18:38:37 +0700

    fix again

--- config_public.js
+++ config_public.js
@@ -5,3 +5,4 @@ var ipServerTourism="http://gissurya.org/tourism_bkt/";*/
 var ipServerAngkot="http://localhost/html/angkot_bkt/";
 var ipServerHotel="http://localhost/html/hotel_bkt/";
 var ipServerTourism="http://localhost/html/tourism_bkt/";
+res.setHeader('Access-Control-Allow-Origin', '*');
