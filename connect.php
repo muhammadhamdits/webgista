a2a02d4f7e90f7924d484d9cbd38015b39f1bd99
MUHAMMAD HAMDI <1711522014@student.unand.ac.id>
2019-10-18 18:33:10 +0700

    fix xhtml

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
 	$conn = mysqli_connect('localhost', 'root', '320819Hana', 'tugas') or die("Gagal");
-	
+	header("Access-Control-Allow-Origin: *");
 	header('Access-Control-Allow-Origin:http://webgista.ddns.net');
 ?>
