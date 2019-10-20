f594698235e016cc3a0c9664a39364edb77e7518
MUHAMMAD HAMDI <1711522014@student.unand.ac.id>
2019-10-20 17:14:37 +0700

    hm

--- postgresql12/connect.php
+++ postgresql12/connect.php
@@ -5,7 +5,7 @@
 	$port = "5432";
 	$dbname = "tugas";
 	$connectionstring = "host=".$host." port=".$port." dbname=".$dbname." user=".$user." password=".$pass;
-	var_dump($connectionstring);
-	die();
 	$conn = pg_connect($connectionstring) or die("Gagal");
+	var_dump($conn);
+	die();
 ?>
