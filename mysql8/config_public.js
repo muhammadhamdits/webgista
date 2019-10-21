0f5f703811dec42e201e15fb6b3e6aeacf2fdaee
MUHAMMAD HAMDI <1711522014@student.unand.ac.id>
2019-10-22 06:27:02 +0700

    memperbaiki ipservertourism

--- mysql8/config_public.js
+++ mysql8/config_public.js
@@ -4,5 +4,5 @@ var ipServerTourism="http://gissurya.org/tourism_bkt/";*/
 
 var ipServerAngkot="http://webgista.ddns.net/angkot_bkt/";
 var ipServerHotel="http://webgista.ddns.net/hotel_bkt/";
-var ipServerTourism="http://webgista.ddns.net/tourism_bkt/";
+var ipServerTourism="http://webgista.ddns.net/mysql8/tourism_bkt/";
 res.setHeader('Access-Control-Allow-Origin', '*');
